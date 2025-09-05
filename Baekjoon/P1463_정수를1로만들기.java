1357e3ea74d5e2247eb38b1ee6e8eae9340defe2
Wish <asgvcx@naver.com>
2025-09-05 10:33:38 +0900

    Create P1463_정수를1로만들기.java

---
+++ Baekjoon/P1463_정수를1로만들기.java
@@ -0,0 +1,30 @@
+/*
+ 점화식 D[i]를 i에서 1로 만드는데 걸리는 최소 연산 횟수로 두고 푼 문제임 
+ */
+
+import java.util.Scanner;
+
+public class P1463_정수를1로만들기 {
+	static int N;
+	static int D[];
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		
+		N = sc.nextInt();
+		D = new int[N+1];
+		D[1]=0;
+		
+		for(int i = 2; i<=N; i++) {
+			D[i] = D[i-1]+1;
+			if(i%2==0)
+				D[i] = Math.min(D[i], D[i/2] + 1);
+			if(i%3==0)
+				D[i] = Math.min(D[i], D[i/3] + 1);
+		}
+		System.out.println(D[N]);
+
+	}
+
+}
